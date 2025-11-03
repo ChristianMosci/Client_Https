d09faefe06074eb8a938ff515d6e994d22e57510
ChristianMosci <mosci.christian@franchettisalviani.net>
2025-11-03 15:30:14 +0100

    Create ChristianMosci_ProgettoClientHttp.java

---
+++ ChristianMosci_ProgettoClientHttp/src/christianmosci_progettoclienthttp/ChristianMosci_ProgettoClientHttp.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package christianmosci_progettoclienthttp;
+
+/**
+ *
+ * @author mosci.christian
+ */
+public class ChristianMosci_ProgettoClientHttp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+          // Creo una istanza della form
+        Form1 form1=new Form1();
+        
+        // Imposto la dimensione della form
+        form1.setSize(580,636);
+        
+        // Rendo visibile la form
+        form1.setVisible(true);
+          
+        // Impedisco il ridimensionamento della form
+        form1.setResizable(false);
+        
+        
+        
+    }
+    
+}
